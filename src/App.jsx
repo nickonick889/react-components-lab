4c8a42221a073a1925977ff8afad4d81d4323822
Nicholas Lai <nicholaslai889@gmail.com>
2026-02-13 11:50:45 +0800

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import WeatherForecast from "./components/WeatherForecast/WeatherForecast.jsx";
 // src/App.jsx
+const weatherForecasts = [
+  {
+    day: "Mon",
+    img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/day.svg",
+    imgAlt: "sun icon",
+    conditions: "sunny",
+    time: "Morning",
+  },
+  {
+    day: "Tue",
+    img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/night.svg",
+    imgAlt: "moon icon",
+    conditions: "clear",
+    time: "Night",
+  },
+  {
+    day: "Wed",
+    img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/stormy.svg",
+    imgAlt: "clouds with lightning icon",
+    conditions: "stormy",
+    time: "All Day",
+  },
+  {
+    day: "Thu",
+    img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/cloudy-day.svg",
+    imgAlt: "sun overcast by clouds icon",
+    conditions: "overcast",
+    time: "Evening",
+  },
+  {
+    day: "Fri",
+    img: "https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-components-lab/assets/cloudy-night.svg",
+    imgAlt: "moon overcast by clouds icon",
+    conditions: "cloudy",
+    time: "Night",
+  },
+];
+
 
 const App = () => {
-
+  console.log(weatherForecasts); 
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Local Weather</h1>
+      <section>
+  {weatherForecasts.map((forecast, index) => (
+    <WeatherForecast 
+      key={index}
+      day={forecast.day} // Matches {day} in child
+      img={forecast.img} // Matches {img} in child
+      imgAlt={forecast.imgAlt} // Matches {imgAlt} in child
+      conditions={forecast.conditions} // Matches {conditions} in child
+      time={forecast.time} // Matches {time} in child
+    />
+  ))}
+</section>
+    </>
   );
 }
 
-export default App
+export default App;
